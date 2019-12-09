a92973ba12db8508e376eef216a607f785480100
Marco Pivetta <ocramius@gmail.com>
2019-12-09 09:16:28 +0100

    Added event sub-namespace dir

---
+++ event/src/Event/Domain/Aggregate/Voucher.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Event\Domain\Aggregate;
+
+use Prooph\EventSourcing\AggregateChanged;
+use Prooph\EventSourcing\AggregateRoot;
+
+final class Event extends AggregateRoot
+{
+    public function aggregateId() : string
+    {
+        throw new \BadFunctionCallException('Not implemented');
+    }
+
+    protected function apply(AggregateChanged $event) : void
+    {
+        throw new \BadFunctionCallException('Not implemented');
+    }
+}
