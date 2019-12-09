0b17a2e3f035f157456fd546063e044b4ad0650f
filipovic.mario@nsoft.ba <filipovic.mario@nsoft.ba>
2019-12-09 11:06:14 +0100

    Removed redundant exception from phpdoc and added return type

--- event/src/Event/Domain/Policy/WhenBetEventCanceled.php
+++ event/src/Event/Domain/Policy/WhenBetEventCanceled.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace Event\Domain\Policy;
 
 use Event\Domain\DomainEvent\EventResultInvalidated;
+use Prooph\Common\Messaging\Command;
 
 final class WhenBetEventCanceled
 {
     /**
-     * @param EventResultInvalidated $eventResultInvalidated
+     * @return Command []
      */
     public function __invoke(EventResultInvalidated $eventResultInvalidated)
     {
--- event/src/Event/Domain/Policy/WhenResultsReceived.php
+++ event/src/Event/Domain/Policy/WhenResultsReceived.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace Event\Domain\Policy;
 
 use Event\Domain\DomainEvent\EventResultReceived;
+use Prooph\Common\Messaging\Command;
 
 final class WhenResultsReceived
 {
     /**
-     * @param EventResultReceived $eventResultReceived
+     * @return Command []
      */
     public function __invoke(EventResultReceived $eventResultReceived)
     {
