45215480b329c43d56f25f3043701303356fdca1
Mirza Cehajic <cehajic.mirza@nsoft.com>
2019-12-09 10:27:40 +0100

    BalanceDecreased event added

---
+++ balance/src/Balance/DomainEvent/BalanceDecreased.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Prooph\EventSourcing\AggregateChanged;
+use Ramsey\Uuid\Uuid;
+
+final class BalanceDecreased extends AggregateChanged
+{
+    public static function withAmount(Uuid $balance, int $amount): self
+    {
+        return self::occur($balance->toString(), ['amount' => $amount]);
+    }
+
+    public function amount() : string
+    {
+        return $this->payload['amount'];
+    }
+}
