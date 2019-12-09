8a86dea9d38704102e719b7dac260fde627e2102
Vladimir Coric <coric.vladimir@outlook.com>
2019-12-09 10:54:43 +0100

    Change serialization

--- ticket/src/Ticket/Domain/DomainEvent/TicketPlaced.php
+++ ticket/src/Ticket/Domain/DomainEvent/TicketPlaced.php
@@ -8,9 +8,9 @@ use Ticket\Domain\Value\TicketAmount;
 
 final class TicketPlaced extends AggregateChanged
 {
-    public static function withAmount(Uuid $ticket, TicketAmount $amount)
+    public static function withAmount(Uuid $ticket, TicketAmount $ticketAmount)
     {
-        self::occur($ticket, ["amount" => $amount->toArray()]);
+        self::occur($ticket, ["amount" => $ticketAmount->amount(), "currency" => $ticketAmount->currency()]);
     }
 
     public function amount() : string
