e4cd5fecdb4d112534903be3e84afdb501927527
Vladimir Coric <coric.vladimir@outlook.com>
2019-12-09 11:23:42 +0100

    Fix aggregate Id passing

--- ticket/src/Ticket/Domain/DomainEvent/TicketPlaced.php
+++ ticket/src/Ticket/Domain/DomainEvent/TicketPlaced.php
@@ -10,7 +10,7 @@ final class TicketPlaced extends AggregateChanged
 {
     public static function withAmount(Uuid $ticket, TicketAmount $ticketAmount)
     {
-        self::occur($ticket, ["amount" => $ticketAmount->amount(), "currency" => $ticketAmount->currency()]);
+        self::occur($ticket->toString(), ["amount" => $ticketAmount->amount(), "currency" => $ticketAmount->currency()]);
     }
 
     public function amount() : TicketAmount
