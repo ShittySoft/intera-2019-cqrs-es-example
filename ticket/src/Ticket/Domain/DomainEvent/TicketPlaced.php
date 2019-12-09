289c6b369093947d48c09cb6ad38f6e526a968b3
Petar Lukic <petar@ito.ba>
2019-12-09 11:28:55 +0100

    Merged ticket placed file

--- ticket/src/Ticket/Domain/DomainEvent/TicketPlaced.php
+++ ticket/src/Ticket/Domain/DomainEvent/TicketPlaced.php
@@ -10,12 +10,7 @@ final class TicketPlaced extends AggregateChanged
 {
     public static function withAmount(Uuid $ticket, TicketAmount $ticketAmount)
     {
-<<<<<<< HEAD
-        self::occur($ticket, ["amount" => $ticketAmount->amount(),
-         "currency" => $ticketAmount->currency()]);
-=======
         self::occur($ticket->toString(), ["amount" => $ticketAmount->amount(), "currency" => $ticketAmount->currency()]);
->>>>>>> b287d9855ede1fd0fe1137deaef90b6191534ee6
     }
 
     public function amount() : TicketAmount
